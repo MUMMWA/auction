ca89730fbdc481ad4ebdcb54601846fa9cb8074d
Medhat Isaac <medhat_isaac@hotmail.com>
2019-09-22 15:33:14 -0500

    Implement the product timer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+// Third party
+import { CountdownModule } from 'ngx-countdown';
+import { AngularMaterialModule } from './material-module';
 
-
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +15,8 @@ import { RegisterComponent } from './register/register.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './_interceptors/jwt.interceptors';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './material-module';
+
+// Services
 import { AuthenticationService } from './_services/authentication.service';
 import { ProductsComponent } from './products/products.component';
 import { TrimPipe } from './_pipes/trim.pipe';
@@ -25,6 +29,7 @@ import { TrimPipe } from './_pipes/trim.pipe';
     RegisterComponent,
     ProductsComponent,
     TrimPipe
+
   ],
   imports: [
     BrowserModule,
@@ -33,6 +38,7 @@ import { TrimPipe } from './_pipes/trim.pipe';
     HttpClientModule,
     AngularMaterialModule,
     BrowserAnimationsModule,
+    CountdownModule
 
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, AuthenticationService],
--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -11,18 +11,28 @@ import { ProductsService } from '../_services/products.service';
 export class ProductsComponent implements OnInit {
 
   constructor(private productsService: ProductsService) { }
+
   products = []
-
-
+  productsArray = []
   ngOnInit() {
     this.productsService.getProducts()
       .subscribe(
         res => {
           this.products = res.products
-          console.log(this.products)
+          this.productsArray = this.products.map(product =>
+            ({
+              ...product,
+              timer: (
+                new Date(product.end_time).getTime() / 1000) - (new Date().getTime() / 1000)
+            })
+          );
+          console.log(this.productsArray);
         },
         error => console.log(error)
       )
+
   }
+
+
 
 }
